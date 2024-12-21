a1d35e08e79ba1096f8e517de611bb33034b806b
T-MSD <dias.tiago03@gmail.com>
2024-12-21 17:43:16 +0000

    Insere objetos e Varios Objetos

--- lib/projeto.pl
+++ lib/projeto.pl
@@ -6,17 +6,15 @@
 % Atenção: não deves copiar nunca os puzzles para o teu ficheiro de código
 % Segue-se o código
 
-visualiza(Lista) :-
-  is_list(Lista),
-  escreve_lista(Lista).
-
-escreve_lista([]).
-escreve_lista([H|T]) :-
+% Visualiza
+visualiza([]).
+visualiza([H|T]) :-
   writeln(H),
-  escreve_lista(T).
+  visualiza(T). 
 
-visualizaLinha(Lista) :-
-  is_list(Lista),      
+
+% Visualiza Linha
+visualizaLinha(Lista) :-      
   print_index(Lista, 1). 
 
 print_index([], _).
@@ -24,3 +22,32 @@ print_index([H|T], N) :-
   format('~d: ~w~n', [N, H]),
   N1 is N + 1,               
   print_index(T, N1).
+
+
+% Insere Objeto
+insereObjecto((L, C), Tabuleiro, Obj) :-
+  nth1(L, Tabuleiro, Linha),
+  nth1(C, Linha, Element),
+  var(Element), !,                                
+  Element = Obj.                    
+insereObjecto(_, _, _).
+
+equal_length(L1, L2) :-
+  length(L1, Size1),
+  length(L2, Size2),
+  Size1 =:= Size2.
+
+
+% Insere Varios Objetos
+insereVariosObjectos(ListaCoords, Tabuleiro, ListaObjs) :-
+  equal_length(ListaCoords, ListaObjs),
+  insereVariosObjectosAux(ListaCoords, Tabuleiro, ListaObjs).
+
+insereVariosObjectosAux([], _, []).
+insereVariosObjectosAux([Coord | RestoCoord], Tabuleiro, [Obj | RestoObj]) :-
+  insereObjecto(Coord, Tabuleiro, Obj),
+  insereVariosObjectosAux(RestoCoord, Tabuleiro, RestoObj).
+
+
+% Insere Pontos à volta
+inserePontosVolta(Tabuleiro, (L, C)) :-
